096d8a4bd31aef7b11e79ce80336aad9958ceb33
Christian <cfrancis1989@gmail.com>
2022-11-28 00:26:30 -0500

    tut-codeapp complete, need my-code

--- tic-tac-toe/tut-code/scripts/app.js
+++ tic-tac-toe/tut-code/scripts/app.js
@@ -1,4 +1,13 @@
+const gameData = [
+  [0, 0, 0],
+  [0, 0, 0],
+  [0, 0, 0],
+];
+
 let editedPlayer = 0;
+let activePlayer = 0;
+let currentRound = 1;
+let gameIsOver = false;
 
 const players = [
   { name: "", symbol: "X" },
@@ -13,20 +22,29 @@ const backdropElement = document.getElementById("backdrop");
 const formElement = document.querySelector("form");
 const errorsOutputElement = document.getElementById("config-errors");
 const gameAreaElement = document.getElementById("active-game");
+const activePlayerNameElement = document.getElementById("active-player-name");
+const gameOverElement = document.getElementById("game-over");
+const winnerNameElement = document.getElementById("winner-name");
 
 const editPlayer1ButtonElement = document.getElementById("edit-player-1-btn");
 const editPlayer2ButtonElement = document.getElementById("edit-player-2-btn");
 const cancelFormButtonElement = document.getElementById("cancel-btn");
 const okAlertButtonElement = document.getElementById("ok-btn");
 const startNewGameButtonElement = document.getElementById("start-game-btn");
+// const gameFieldElements = document.querySelectorAll("#game-board li");
+const gameBoardElement = document.getElementById("game-board");
 
-editPlayer1ButtonElement.addEventListener("click", openPlayerConfig);
-editPlayer2ButtonElement.addEventListener("click", openPlayerConfig);
-
+backdropElement.addEventListener("click", closeAllModals);
 cancelFormButtonElement.addEventListener("click", closePlayerConfig);
 okAlertButtonElement.addEventListener("click", closeAlertSetPlayerOverlay);
-backdropElement.addEventListener("click", closeAllModals);
+editPlayer1ButtonElement.addEventListener("click", openPlayerConfig);
+editPlayer2ButtonElement.addEventListener("click", openPlayerConfig);
 
 formElement.addEventListener("submit", savePlayerConfig);
 
 startNewGameButtonElement.addEventListener("click", startNewGame);
+
+// for (const gameFieldElement of gameFieldElements) {
+//   gameFieldElement.addEventListener("click", selectGameField);
+// }
+gameBoardElement.addEventListener("click", selectGameField);
--- tic-tac-toe/tut-code/scripts/game.js
+++ tic-tac-toe/tut-code/scripts/game.js
@@ -1,5 +1,128 @@
+function resetGameStatus() {
+  activePlayer = 0;
+  currentRound = 1;
+  gameIsOver = false;
+  gameOverElement.firstElementChild.innerHTML =
+    'You won, <span id="winner-name">PLAYER NAME</span>!';
+  gameOverElement.style.display = "none";
+
+  let gameBoardIndex = 0;
+  for (let i = 0; i < 3; i++) {
+    for (let j = 0; j < 3; j++) {
+      gameData[i][j] = 0;
+      const gameBoardItemElement = gameBoardElement.children[gameBoardIndex];
+      gameBoardItemElement.textContent = "";
+      gameBoardItemElement.classList.remove("disabled");
+      gameBoardIndex++;
+    }
+  }
+}
+
 function startNewGame() {
-  players[0].name && players[1].name
-    ? (gameAreaElement.style.display = "block")
-    : openAlertSetPlayersOverlay();
+  if (players[0].name && players[1].name) {
+    resetGameStatus();
+
+    activePlayerNameElement.textContent = players[activePlayer].name;
+    gameAreaElement.style.display = "block";
+    return;
+  }
+  openAlertSetPlayersOverlay();
+}
+
+function switchPlayer() {
+  if (activePlayer === 0) {
+    activePlayer = 1;
+  } else {
+    activePlayer = 0;
+  }
+  activePlayerNameElement.textContent = players[activePlayer].name;
+}
+
+// function selectGameField(e) {
+//   e.target.textContent = players[activePlayer].symbol;
+//   e.target.classList.add("disabled");
+//   switchPlayer();
+// }
+function selectGameField(e) {
+  if (e.target.tagName !== "LI" || gameIsOver) {
+    return;
+  }
+  const selectedField = e.target;
+  const selectedColumn = selectedField.dataset.col - 1;
+  const selectedRow = selectedField.dataset.row - 1;
+
+  if (gameData[selectedRow][selectedColumn] > 0) {
+    alert("Please select an empty field!");
+    return;
+  }
+
+  selectedField.textContent = players[activePlayer].symbol;
+  selectedField.classList.add("disabled");
+
+  gameData[selectedRow][selectedColumn] = activePlayer + 1;
+
+  const winnerId = checkForGameOver();
+  if (winnerId !== 0) {
+    endGame(winnerId);
+  }
+
+  currentRound++;
+  switchPlayer();
+}
+
+function checkForGameOver() {
+  for (let i = 0; i < 3; i++) {
+    if (
+      gameData[i][0] > 0 &&
+      gameData[i][0] === gameData[i][1] &&
+      gameData[i][1] === gameData[i][2]
+    ) {
+      return gameData[i][0];
+    }
+  }
+
+  for (let i = 0; i < 3; i++) {
+    if (
+      gameData[0][i] > 0 &&
+      gameData[0][i] === gameData[1][i] &&
+      gameData[1][i] === gameData[2][i]
+    ) {
+      return gameData[0][i];
+    }
+  }
+
+  if (
+    gameData[0][0] > 0 &&
+    gameData[0][0] === gameData[1][1] &&
+    gameData[1][1] === gameData[2][2]
+  ) {
+    return gameData[0][0];
+  }
+
+  if (
+    gameData[2][0] > 0 &&
+    gameData[2][0] === gameData[1][1] &&
+    gameData[1][1] === gameData[0][2]
+  ) {
+    return gameData[2][0];
+  }
+
+  if (currentRound === 9) {
+    return -1;
+  }
+
+  return 0;
+}
+
+function endGame(winnerId) {
+  gameOverElement.style.display = "block";
+  gameIsOver = true;
+
+  if (winnerId > 0) {
+    gameOverElement.firstElementChild.firstElementChild.textContent =
+      players[winnerId - 1].name;
+    // winnerNameElement.textContent = players[winnerId - 1].name;
+  } else {
+    gameOverElement.firstElementChild.textContent = "It's a draw!";
+  }
 }
