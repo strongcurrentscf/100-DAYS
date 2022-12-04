e03dd29f32899c1ca92475b5cad9a756bfefd7bf
strongcurrentscf <cfrancis1989@gmail.com>
2022-12-04 16:11:16 -0500

    config js complete needs game js

--- tic-tac-toe/recall-code/scripts/app.js
+++ tic-tac-toe/recall-code/scripts/app.js
@@ -1,13 +1,31 @@
-//////////
+let playerToEdit = 0;
+
+const players = [
+  { name: "", symbol: "X" },
+  { name: "", symbol: "O" },
+];
+
+//////////elements
 const configPlayerFormOverlayEl = document.getElementById("config-overlay");
+const setPlayersAlertOverlayEl = document.getElementById(
+  "set-players-alert-overlay"
+);
 const backdropEl = document.getElementById("backdrop");
+const modalEl = document.querySelector(".modal");
 const formEl = document.querySelector("form");
+const configErrorMessage = document.getElementById("config-error-message");
 
 //////////buttons
 const editPlayer1NameEl = document.getElementById("edit-player-1-btn");
 const editPlayer2NameEl = document.getElementById("edit-player-2-btn");
 const configPlayerCancelBtnEl = document.getElementById("cancel-btn");
 const startNewGameBtnEl = document.getElementById("start-new-game-btn");
+const cancelPlayerConfigBtnEl = document.getElementById("cancel-btn");
 
 /////////listeners
-startNewGameBtnEl.addEventListener("click", openPlayerConfig);
+editPlayer1NameEl.addEventListener("click", openPlayerConfig);
+editPlayer2NameEl.addEventListener("click", openPlayerConfig);
+backdropEl.addEventListener("click", closeModals);
+cancelPlayerConfigBtnEl.addEventListener("click", closeModals);
+formEl.addEventListener("submit", savePlayerConfigData);
+startNewGameBtnEl.addEventListener("click", openSetPlayersAlert);
--- tic-tac-toe/recall-code/scripts/config.js
+++ tic-tac-toe/recall-code/scripts/config.js
@@ -1,4 +1,50 @@
-function openPlayerConfig() {
+function openPlayerConfig(e) {
+  playerToEdit = +e.target.dataset.playerid;
   backdropEl.style.display = "block";
   configPlayerFormOverlayEl.style.display = "block";
+
+  console.log(playerToEdit);
+}
+
+function closePlayerConfig() {
+  backdropEl.style.display = "none";
+  configPlayerFormOverlayEl.style.display = "none";
+  formEl.firstElementChild.classList.remove("error");
+  formEl.firstElementChild.lastElementChild.value = "";
+  configErrorMessage.textContent = "";
+}
+
+function openSetPlayersAlert() {
+  backdropEl.style.display = "block";
+  setPlayersAlertOverlayEl.style.display = "block";
+}
+function closeSetPlayersAlert() {
+  backdropEl.style.display = "none";
+  setPlayersAlertOverlayEl.style.display = "none";
+}
+
+function closeModals() {
+  closePlayerConfig();
+  closeSetPlayersAlert();
+}
+
+function savePlayerConfigData(e) {
+  e.preventDefault();
+  const formData = new FormData(formEl);
+  const enteredPlayerName = formData.get("playername").trim().toUpperCase();
+
+  if (!enteredPlayerName) {
+    e.target.firstElementChild.classList.add("error");
+    configErrorMessage.textContent = "Please enter a valid name!";
+    return;
+  }
+
+  const playerElementToUpdate = document.getElementById(
+    "player-" + playerToEdit + "-data"
+  );
+
+  playerElementToUpdate.children[1].textContent = enteredPlayerName;
+  players[playerToEdit - 1].name = enteredPlayerName;
+
+  closePlayerConfig();
 }
