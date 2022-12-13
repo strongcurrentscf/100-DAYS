302f923fb2813efa45ec2f5d7015ccecb3c11804
Christian <cfrancis1989@gmail.com>
2022-12-12 19:52:51 -0500

    expressJS basics complete

--- node-js/tut-code/scripts/app.js
+++ node-js/tut-code/scripts/app.js
@@ -1,7 +1,6 @@
 const fs = require("fs");
 const path = require("path");
 const express = require("express");
-const { resolveSoa } = require("dns");
 
 const app = express();
 app.use(express.urlencoded({ extended: false }));
