b9c12e4747365b40672dd8c380c25ecf3034009a
strongcurrentscf <cfrancis1989@gmail.com>
2023-01-07 23:07:37 -0500

    added view/edit post functionality/routing

--- express-mysql-s24/routes/blog.js
+++ express-mysql-s24/routes/blog.js
@@ -44,4 +44,71 @@ router.post("/posts", async function (req, res) {
   res.redirect("/posts");
 });
 
+router.get("/posts/:id", async function (req, res) {
+  const postId = req.params.id;
+
+  const query = `
+    SELECT posts.*, authors.name AS author_name, authors.email AS author_email FROM posts
+    INNER JOIN authors ON posts.author_id = authors.id
+    WHERE posts.id = ${postId}
+  `;
+  const [[post]] = await db.query(query);
+
+  if (!post) {
+    return res.status(404).render("404");
+  }
+
+  console.log(post);
+
+  res.render("post-detail", { post: post });
+});
+// router.get("/posts/:id", async function (req, res) {
+//   const query = `
+//         SELECT * FROM posts WHERE id = ?
+//     `;
+
+//   const [[post]] = await db.query(query, [req.params.id]);
+
+//   console.log(post);
+
+//   if (!post) {
+//     return res.status(404).render("404");
+//   }
+
+//   res.render("post-detail", { post: post });
+// });
+
+router.get("/update-post/:id", async function (req, res) {
+  const postId = req.params.id;
+  const query = `
+    SELECT title, summary, body FROM blog.posts 
+    WHERE posts.id = ${postId}
+  `;
+  const [[post]] = await db.query(query);
+
+  console.log(post);
+
+  res.render("update-post", { post: post });
+});
+
+router.post("/update-post/:id", async function (req, res) {
+  const postId = req.params.id;
+  const [title, summary, body] = [
+    req.body.title,
+    req.body.summary,
+    req.body.body,
+  ];
+
+  const query = `
+    UPDATE blog.posts 
+    SET title= "${title}", summary= "${summary}", body= "${body}"
+    WHERE blog.posts.id = ${postId}
+  `;
+  //     const [[post]] = await db.query(query);
+
+  //   console.log(post);
+
+  //   res.render("post-detail", { post: post });
+});
+
 module.exports = router;
