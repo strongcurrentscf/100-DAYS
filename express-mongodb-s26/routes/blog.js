999ed3317d6cffb9b6c93edaab35442fdfd168b0
strongcurrentscf <cfrancis1989@gmail.com>
2023-01-11 18:55:40 -0500

    inserted new post with create-post, passed form data,  di to dbslayed posts-list

--- express-mongodb-s26/routes/blog.js
+++ express-mongodb-s26/routes/blog.js
@@ -1,17 +1,54 @@
-const express = require('express');
+const express = require("express");
+const mongodb = require("mongodb");
+
+const db = require("../data/database");
+const ObjectId = mongodb.ObjectId;
 
 const router = express.Router();
 
-router.get('/', function(req, res) {
-  res.redirect('/posts');
+router.get("/", function (req, res) {
+  res.redirect("/posts");
 });
 
-router.get('/posts', function(req, res) {
-  res.render('posts-list');
+router.get("/posts", async function (req, res) {
+  const posts = await db.getDb().collection("posts").find().toArray();
+  console.log(posts);
+
+  res.render("posts-list", { posts: posts });
 });
 
-router.get('/new-post', async function(req, res) {
-  res.render('create-post');
+router.get("/new-post", async function (req, res) {
+  const authors = await db.getDb().collection("authors").find().toArray();
+  // console.log(authors);
+
+  res.render("create-post", { authors: authors });
+});
+
+router.post("/new-post", async function (req, res) {
+  const authorId = new ObjectId(req.body.author);
+  const author = await db
+    .getDb()
+    .collection("authors")
+    .findOne({ _id: authorId });
+
+  const newPost = {
+    title: req.body.title,
+    summary: req.body.summary,
+    body: req.body.content,
+    date: new Date(),
+    author: {
+      id: authorId,
+      name: author.name,
+      email: author.email,
+    },
+  };
+
+  const result = await db.getDb().collection("posts").insertOne(newPost);
+
+  console.log(result);
+  console.log(newPost);
+
+  res.redirect("/posts");
 });
 
 module.exports = router;
