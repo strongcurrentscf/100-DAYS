9f4459f378cb02d2d75c20dbc31ee3bf3e482bac
strongcurrentscf <cfrancis1989@gmail.com>
2023-01-14 13:51:06 -0500

    added try/catch to handle ObjectId error in "/posts/:id"

--- express-mongodb-s26/routes/blog.js
+++ express-mongodb-s26/routes/blog.js
@@ -50,15 +50,23 @@ router.post("/posts", async function (req, res) {
 });
 
 router.get("/posts/:id", async function (req, res) {
-  const postId = new ObjectId(req.params.id);
+  let postId = req.params.id;
+
+  try {
+    postId = new ObjectId(postId);
+  } catch (error) {
+    return res.status(404).render("404");
+  }
+
   const post = await db
     .getDb()
     .collection("posts")
     .findOne({ _id: postId }, { projection: { summary: 0 } });
+  // .findOne({ _id: postId });
 
-  if (!post) {
-    return res.status(404).render("404");
-  }
+  // if (!post) {
+  //   return res.status(404).render("404");
+  // }
 
   post.humanReadableDate = post.date.toLocaleDateString("en-US", {
     weekday: "long",
