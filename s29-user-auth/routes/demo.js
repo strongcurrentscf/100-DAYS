5f794a39b3281fcb9e6c4e282085e1fb058c7eba
Christian <cfrancis1989@gmail.com>
2023-01-23 18:46:14 -0500

    added bcryptjs hashing to signup password/instantiated sessionStore in app.js

--- s29-user-auth/routes/demo.js
+++ s29-user-auth/routes/demo.js
@@ -2,6 +2,8 @@ const express = require("express");
 
 const db = require("../data/database");
 
+const bcrypt = require("bcryptjs");
+
 const router = express.Router();
 
 router.get("/", function (req, res) {
@@ -16,9 +18,73 @@ router.get("/login", function (req, res) {
   res.render("login");
 });
 
-router.post("/signup", async function (req, res) {});
+router.post("/signup", async function (req, res) {
+  const enteredEmail = req.body.email;
+  const enteredConfirmEmail = req.body["confirm-email"];
+  const enteredPassword = await bcrypt.hash(req.body.password, 12);
 
-router.post("/login", async function (req, res) {});
+  if (
+    !enteredEmail ||
+    !enteredConfirmEmail ||
+    !enteredPassword ||
+    enteredPassword.trim() < 6 ||
+    enteredEmail !== enteredConfirmEmail ||
+    !enteredEmail.includes("@")
+  ) {
+    console.log("INCORRECT DATA");
+    return res.redirect("/signup");
+  }
+
+  const user = {
+    email: enteredEmail,
+    confirmEmail: enteredConfirmEmail,
+    password: enteredPassword,
+  };
+
+  const existingUser = await db
+    .getDb()
+    .collection("users")
+    .findOne({ email: user.email });
+
+  if (existingUser) {
+    console.log("USER EXISTS ALREADY!");
+    return res.redirect("/signup");
+  }
+
+  await db.getDb().collection("users").insertOne(user);
+
+  res.redirect("/login");
+});
+
+router.post("/login", async function (req, res) {
+  const enteredEmail = req.body.email;
+  const enteredPassword = req.body.password;
+
+  const user = {
+    email: enteredEmail,
+    password: enteredPassword,
+  };
+
+  const existingUser = await db
+    .getDb()
+    .collection("users")
+    .findOne({ email: user.email });
+
+  if (!existingUser) {
+    return res.redirect("/login");
+  }
+
+  const matchingPassword = await bcrypt.compare(
+    user.password,
+    existingUser.password
+  );
+
+  if (!matchingPassword) {
+    return res.redirect("/login");
+  }
+
+  res.redirect("/admin");
+});
 
 router.get("/admin", function (req, res) {
   res.render("admin");
