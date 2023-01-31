c4478414a65b92849551a75b7b0872ae3e827cce
strongcurrentscf <cfrancis1989@gmail.com>
2023-01-31 00:29:24 -0500

    need to add csurf package, setup csrf middleware, and add hidden csrf form inputs

--- s30-security/csrf/my-site/app.js
+++ s30-security/csrf/my-site/app.js
@@ -3,6 +3,7 @@ const path = require("path");
 const express = require("express");
 const session = require("express-session");
 const mongodbStore = require("connect-mongodb-session");
+const csrf = require("csurf");
 
 const db = require("./data/database");
 const demoRoutes = require("./routes/demo");
